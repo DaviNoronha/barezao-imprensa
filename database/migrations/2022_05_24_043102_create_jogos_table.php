75080d90b0c076f349294bd0fa15fbe0237fdf55
Davi Noronha <davinoronha.g@gmail.com>
2022-05-24 00:52:21 -0400

    criando models, migrations e controllers

---
+++ database/migrations/2022_05_24_043102_create_jogos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJogosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jogos', function (Blueprint $table) {
+            $table->id();
+            $table->date('data');
+            $table->unsignedBigInteger('casa_id');
+            $table->unsignedBigInteger('fora_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('casa_id')->references('id')->on('times');
+            $table->foreign('fora_id')->references('id')->on('times');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jogos');
+    }
+}
--- database/migrations/2022_05_24_043154_create_jogadores_table.php
+++ database/migrations/2022_05_24_043154_create_jogadores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateJogadoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('jogadores', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('empresa');
+            $table->string('comprovante');
+            $table->unsignedBigInteger('time_id');
             $table->timestamps();
+
+            $table->foreign('time_id')->references('id')->on('times');
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jogadores');
     }
 }
