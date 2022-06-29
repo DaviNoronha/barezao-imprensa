5c1cafab5391ffd8ba07b3d1bbf4cb5090b3925c
Davi Noronha <davinoronha.g@gmail.com>
2022-06-28 21:48:32 -0400

    add tipo e empresa no show de jogador

--- resources/views/jogadores/show.blade.php
+++ resources/views/jogadores/show.blade.php
@@ -19,11 +19,16 @@
                                 <img src="{{ asset('storage') . '/' . $jogador->foto }}" class="w-50" alt="">
                             </div>
                             <div class="col-md-6">
-                                <h1>{{$jogador->nome}}</h1>
-                                <h2>Função: {{$jogador->funcao}}</h2>
-                                <h2>Time: {{$jogador->time->time}}</h2>
-                                <h3>Nome na camisa: {{$jogador->nome_camisa}}</h3>
-                                <h3>Número da camisa: {{$jogador->numero}}</h3>
+                                <h1 class="mb--1">{{$jogador->nome}}</h1>
+                                <h4>{{$jogador->tipo == '0' ? 'Imprensa' : 'Estrangeiro'}}</h4>
+                                <hr class="mt--1">
+                                <ul>
+                                    <li><h2><b>Time:</b> {{$jogador->time->time}}</h2></li>
+                                    <li><h2><b>Empresa:</b> {{$jogador->time->empresa}}</h2></li>
+                                    <li><h2><b>Função:</b> {{$jogador->funcao}}</h2></li>
+                                    <li><h2><b>Nome na camisa:</b> {{$jogador->nome_camisa}}</h2></li>
+                                    <li><h2><b>Número da camisa:</b> {{$jogador->numero}}</h2></li>
+                                </ul>
                             </div>
                         </div>
                     </div>
